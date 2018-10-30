f4aab683d058fb89a61587bdb13eae8c7c46da1c
Juan <juan.alvarez@escuelaestech.es>
2018-10-30 11:44:46 +0100

    Ejercicio 13 de blucles y ArraysList Terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,36 +2,30 @@ package com.company;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.ArrayList;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Ejercicio 12 ArraysList");
-        System.out.println("Ordenación de los números de un vector");
+        System.out.println("Ejercicio 13 ArraysList");
+        System.out.println("Ver si el número introeducido esta en el array y mostrar su posicion");
         Scanner sc =new Scanner(System.in);
 
         int[] array={4,3,2,1};
-        int[] array1={3,2,1,4};
+        System.out.println("Introduzca el número a buscar");
+        int num = sc.nextInt();
+
 
          ArrayList<Integer> list = new ArrayList<>();
 
          for (int i=0;i<array.length;i++) {
              list.add(array[i]);
          }
-         Collections.sort(list);
-        System.out.println(list);
-
-
-
-
-
-
-
-
-
-
-
+         boolean exist = list.contains(num);
+         if (exist==true){
+             System.out.println("La posición existente del número indicado es: "+ list.indexOf(num));
+         } else System.out.println("El número indicado no existe en el array");
 
     }
 
